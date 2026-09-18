a3e90658fec88b3be0cf6519edafa902cc8681c1
kirwarooney <rooneykirwokin56@gmail.com>
2026-09-19 02:05:13 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Delivery app backend is running!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
